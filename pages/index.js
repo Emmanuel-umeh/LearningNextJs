4883e96d5784af7c5f44400a676a9c18d5396af0
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2020-09-02 11:29:47 +0100

    s

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,22 @@
+import Link from "next/link";
+import Fetch from "isomorphic-unfetch";
+import Navbar from "../components/navbar";
+import Layout from "../components/layout";
+import Prices from "../components/prices";
+const index = (props) => (
 
-import Link from "next/link"
+  <Layout>
+    <h1 style={{ textAlign: "center" }}>Welcome to Next JS</h1>
+    <Prices bpi={props.bpi} />
+  </Layout>
+);
 
-import Navbar from "../components/navbar"
-import Layout from "../components/layout"
-const index = ()=>(
-
-<Layout>
-
-
-
-
-
-    <h1 style ={{textAlign : "center"}}>
-        Home Page
-    </h1>
-
-
-</Layout>)
-
-export default index
+index.getInitialProps = async function () {
+  const res = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
+  const data = await res.json();
+  // console.log("data ", data)
+  return {
+    bpi: data.bpi,
+  };
+};
+export default index;
